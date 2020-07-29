42c83d4f3e37a9651d6330fb943cf93b5529a951
vladpacc <vladpylypenko13@gmail.com>
2020-07-29 15:11:30 -0500

    update

---
+++ jenkinsDeploy.groovy
@@ -0,0 +1,10 @@
+properties([
+    parameters([
+        booleanParam(defaultValue: false, description: 'please select to apply the changes', name: 'terraformApply '),
+        booleanParam(defaultValue: false, description: 'Please select to Destroy everything', name: 'terraformDestroy'),
+        booleanParam(defaultValue: false, description: 'Please select the job to run in debugMode', name: 'debugMode'),
+        choice(choices: ['dev', 'qa', 'stage', 'prod'], description: 'Please select the environment to deploy.', name: 'environment')
+        ])
+     ])
+
+println('Hello world')
