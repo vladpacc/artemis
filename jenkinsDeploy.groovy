69d3f05e7119570c8385ad65dcc874bd62bce9ea
vladpacc <vladpylypenko13@gmail.com>
2020-07-31 23:16:14 -0500

    generate sh command

--- jenkinsDeploy.groovy
+++ jenkinsDeploy.groovy
@@ -67,6 +67,7 @@ def slavePodTemplate = """
             """.stripIndent()
             writeFile file: 'deployment_configuration.tfvars', text: "${deployment_configuration_tfvars}"
             sh 'cat deployment_configuration.tfvars >> dev.tfvars'
+            sh /scripts/Dockerfile/set-config.sh
           }   
         }
         container("buildtools") {
